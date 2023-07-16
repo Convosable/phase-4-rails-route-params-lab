f181e415780f49468831746809875741dca0b952
Connor Vosberg <connorjv@gmail.com>
2023-07-16 08:24:56 -0700

    commit tree 6c73cc44199fa2d10a3e983511b2eaf04a116b2d

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,15 @@
 class Student < ApplicationRecord
 
+  scope :by_name, -> (name) { where("first_name LIKE ? OR last_name LIKE ?", "%#{name}%", "%#{name}%") }
+
   def to_s
     "#{first_name} #{last_name}"
   end
+
+  # scope method above is the same as the instance method below 
+
+  # def self.by_name(name)
+  #   Student.where("first_name LIKE ? OR last_name LIKE ?", "%#{name}%", "%#{name}%")
+  # end
 
 end
